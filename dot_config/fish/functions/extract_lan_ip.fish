e147e3ce97f2d14584a2efdefee67ee45bb02f59
ktaki8ra <takihara@rabbix.jp>
2023-09-01 12:59:50 +0900

    fix cond of extracting lanip

--- dot_config/fish/functions/extract_lan_ip.fish
+++ dot_config/fish/functions/extract_lan_ip.fish
@@ -1,8 +1,8 @@
 function extract_lan_ip
     set lan_ip (ifconfig | grep 192.168 | awk '{print $2}' | head -n 1)
-    if test "$status" -ne 0
-        return
-    else
+    if test -n "$lan_ip"
         echo "$lan_ip"
+    else
+        echo "no_lan"
     end
 end
