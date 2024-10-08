ced895e5d229a2d5c6259317a77772f062db5e1b
ktaki8ra <takihara@rabbix.jp>
2024-10-08 17:47:53 +0900

    add sdkman and path for it

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -14,3 +14,11 @@ if status is-interactive
     source (nodenv init -|psub)
     source (rbenv init -|psub)
 end
+
+# sdkman
+function sdk
+    bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && sdk $argv"
+end
+for ITEM in $HOME/.sdkman/candidates/* ;
+    set -gx PATH $PATH $ITEM/current/bin
+end
---
+++ install_sdkman.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# ref: https://sdkman.io/
+curl -s "https://get.sdkman.io" | bash
