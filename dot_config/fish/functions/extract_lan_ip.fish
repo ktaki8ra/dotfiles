75b5a66a167fc2a6181f01227233e098608c936a
ktaki8ra <takihara@rabbix.jp>
2025-10-16 23:49:30 +0900

    update: show the IP address assigned by the VPN, if connected via it

--- dot_config/fish/functions/extract_lan_ip.fish
+++ dot_config/fish/functions/extract_lan_ip.fish
@@ -1,7 +1,12 @@
 function extract_lan_ip
     set lan_ip (ifconfig | grep -E '192.168.10.|192.0.1.' | awk '{print $2}' | head -n 1)
+    set vpn_ip (ifconfig -a | grep "\-\->" | awk '{print $2}' | head -n 1)
     if test -n "$lan_ip"
-        echo "$lan_ip"
+        if test -n "$vpn_ip"
+            echo "$lan_ip#$vpn_ip"(set_color normal)'('(set_color green)'vpn'(set_color normal)')'
+        else
+            echo "$lan_ip"
+        end
     else
         echo "no_lan"
     end
