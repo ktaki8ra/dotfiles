39077df185426bdecae5f78bf27d08c15806bccc
ktaki8ra <takihara@rabbix.jp>
2025-10-16 20:11:15 +0900

    update: extract_lan_ip

--- dot_config/fish/functions/extract_lan_ip.fish
+++ dot_config/fish/functions/extract_lan_ip.fish
@@ -1,5 +1,5 @@
 function extract_lan_ip
-    set lan_ip (ifconfig | grep 192. | awk '{print $2}' | head -n 1)
+    set lan_ip (ifconfig | grep -E '192.168.10.|192.0.1.' | awk '{print $2}' | head -n 1)
     if test -n "$lan_ip"
         echo "$lan_ip"
     else
