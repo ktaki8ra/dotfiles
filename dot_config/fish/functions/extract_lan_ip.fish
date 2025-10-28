b51793a03fb0af370078d79f4ebe453341559007
ktaki8ra <takihara@rabbix.jp>
2025-10-28 20:19:47 +0900

    update: returns the IP of iface, which status is active

--- dot_config/fish/functions/extract_lan_ip.fish
+++ dot_config/fish/functions/extract_lan_ip.fish
@@ -1,13 +1,13 @@
 function extract_lan_ip
-    set lan_ip (ifconfig | grep -E '192.168.10.|192.0.1.' | awk '{print $2}' | head -n 1)
-    set vpn_ip (ifconfig -a | grep "\-\->" | awk '{print $2}' | head -n 1)
-    if test -n "$lan_ip"
-        if test -n "$vpn_ip"
-            echo "$lan_ip#$vpn_ip"(set_color normal)'('(set_color green)'vpn'(set_color normal)')'
-        else
-            echo "$lan_ip"
-        end
+    set iface (ifconfig | awk '/flags/ {iface=$1} /status: active/ {print iface; exit}' | sed 's/://')
+    if test -n "$iface"
+        set lan_ip (ifconfig "$iface" | grep "inet " | awk '{print $2}')
     else
-        echo "no_lan"
+        set lan_ip "no_lan"
+    end
+    printf "$lan_ip"
+    set vpn_ip (ifconfig -a | grep "\-\->" | awk '{print $2}' | head -n 1)
+    if test -n "$vpn_ip"
+        printf "#$vpn_ip"(set_color normal)'('(set_color green)'vpn'(set_color normal)')'
     end
 end
